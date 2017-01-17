79ac45a45198e3af1865843504b9ba974bf94d3c
unknown <3165892775@qq.com>
2017-01-17 11:23:00 +0800

    add comment.

--- LRU算法/LRUListNode.h
+++ LRU算法/LRUListNode.h
@@ -6,19 +6,20 @@ using namespace std;
 
 
 template <typename T,typename E>
-struct CacheNode
+struct CacheNode  //链表中的节点
 {
-    T key;
-    E value;
+    T key;        //键
+    E value;      //值
     CacheNode(T k,E val):key(k),value(val){};
-    CacheNode<T,E>* pre;
-    CacheNode<T,E>* next;
+    CacheNode<T,E>* pre;      //指向前驱节点
+    CacheNode<T,E>* next;     //指向后续节点
 };
 
 template <typename T,typename E>
-class LRUChacheList
+class LRUChacheList     //LRU类
 {
 public:
+    //构造函数
     LRUChacheList(int sz,T key,E val):mSize(sz)
     {
         mHead = new  CacheNode<T,E>(key,val);
@@ -26,16 +27,36 @@ public:
         mHead->pre = nullptr;
         mCurrentSize = 0;
     }
+
+    //虚析构函数
     virtual ~LRUChacheList()
     {
+         if (nullptr == mHead->next) //只有头结点
+         {
+             delete mHead;
+
+         }else //有多个节点
+         {
+            CacheNode<T,E>* p = mHead->next;
+            CacheNode<T,E>* q = nullptr;
+            while (p != mHead)
+            {
+                q = p;
+                p = p->next;
+                delete q;
+            }
+            delete mHead;
+         }
 
     }
+
+    //设置新值
     void setKey(T key,E value)
     {
         CacheNode<T,E>* ret=nullptr;
-        if (findKey(key,ret) == false)
+        if (findKey(key,ret) == false) //当前cache没有对应键的值
         {
-            if (mCurrentSize == mSize)
+            if (mCurrentSize == mSize)  //cache已经满 则删除链表尾部的节点
             {
                  CacheNode<T,E>* p = mHead->pre;
                  CacheNode<T,E>* q = p->pre;
@@ -44,9 +65,11 @@ public:
                  delete p;
                  --mCurrentSize;
             }
+
+            //新建节点 并插入到链表头部
             CacheNode<T,E>* s = new  CacheNode<T,E>(key,value);
             ++mCurrentSize;
-            if (mHead->next == nullptr)
+            if (mHead->next == nullptr)  //第一次插入节点时
             {
                 mHead->next = s;
                 s->pre = mHead;
@@ -54,12 +77,14 @@ public:
                 mHead->pre = s;
                 return;
             }
+
+            //第二次以后插入节点
             mHead->next->pre = s;
             s->next = mHead->next;
             mHead->next = s;
             s->pre = mHead;
 
-        }else
+        }else  //如果找到对应的键值 则更新它的值
         {
             if(nullptr != ret)
             {
@@ -75,10 +100,12 @@ public:
             }
         }
     }
+
+    //获取对应键的值
     bool getValue(T& key,E& val)
     {
          CacheNode<T,E>* ret=nullptr;
-         if(true == findKey(key,ret))
+         if(true == findKey(key,ret))  //找到
          {
              if(nullptr != ret)
             {
@@ -94,9 +121,12 @@ public:
             }
             return true;
          }
+
+         //没找到
          return false;
     }
 
+    //打印cache中的节点的值
     void print()
     {
         if (nullptr == mHead)
@@ -111,12 +141,14 @@ public:
         cout << endl;
     }
 
+    //重载<<
     ostream& operator<< (CacheNode<T,E>& s)
     {
         return cout<<s.value;
     }
 
 protected:
+    //找cache中是否存在对应的键 若存在将节点通过参数s返回 函数返回true
     bool findKey(T key,CacheNode<T,E>*& s)
     {
         if (nullptr == mHead->next)
@@ -137,6 +169,7 @@ protected:
         return false;
     }
 
+    //找值 并通过参数s返回改节点
     bool findValue(E val,CacheNode<T,E>*& s)
     {
         if (mHead->next == nullptr)
@@ -155,9 +188,9 @@ protected:
         return false;
     }
 private:
-    int mSize;
-    int mCurrentSize;
-    CacheNode<T,E>* mHead;
+    int mSize;           //cache的总容量
+    int mCurrentSize;     //cache的当前容量
+    CacheNode<T,E>* mHead;   //链表的头结点 用于管理链表
 };
 
 
